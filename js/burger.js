d6e36dddce9f93896f0b15293adf1599c811cdfe
frontdevoloper <frontdevop@gmail.com>
2021-07-04 21:42:30 +0200

    пример

---
+++ js/burger.js
@@ -0,0 +1,6 @@
+let btn = document.querySelector(".menu__button");
+let menuBar = document.querySelector(".header__menu-bar");
+
+btn.addEventListener("click", () => {
+  menuBar.classList.toggle("active");
+});
